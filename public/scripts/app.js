f3258a3b5e1b686ef9120d460c8f2ce74ce32ab4
PreranaShrestha <prerana.sh@gmail.com>
2018-02-09 00:02:29 +0000

    package.json

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,22 @@
 $(() => {
+
+   var dateChosen = [];
+      $(document).ready(function(){
+        $('#calendar').fullCalendar({
+          dayClick: function(date, jsEvent, view) {
+            $(this).closest('td').toggleClass('down');
+            let index = dateChosen.indexOf(date.format('YYYY-MM-DD'));
+            if (index > -1) {
+              dateChosen.splice(index, 1);
+            } else {
+              dateChosen.push(date.format('YYYY-MM-DD'));
+            }
+            for(var date of dateChosen){
+              console.log(date);
+            }
+          }
+        });
+      });
   $.ajax({
     method: "GET",
     url: "/api/users"
