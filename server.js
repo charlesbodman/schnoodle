c3519abde571732ea426a1dee56d40a735abb35c
DercilioFontes <dercilioafontes@gmail.com>
2018-02-10 21:52:57 +0000

    Implemented getting the event data form DB and sending to events_show.ejs.

--- server.js
+++ server.js
@@ -51,16 +51,17 @@ app.get("/", (req, res) => {
 
 // Home page
 app.get("/events/:id", (req, res) => {
-    // const dataEventAndSlots = knex('events').where('url', `http://localhost:8080/events/${req.params.id}`).select('title', 'description', 'location', 'organizer_name', 'organizer_email')
-     knex('events').select()
+    const dataEventAndSlots = knex('events').where('url', `http://localhost:8080/events/${req.params.id}`).select('title', 'description', 'location', 'organizer_name', 'organizer_email')
     .then(function(result) {
+      const templateVar = result;
       console.log(result);
       console.log("success in getting event data from DB!");
+      res.render("events_show", templateVar[0]);
     } );
 
-    console.log(dataEventAndSlots);
+    // console.log(dataEventAndSlots[0]);
 
-  res.render("events_show", dataEventAndSlots);
+  // res.render("events_show", dataEventAndSlots[0]);
 });
 
 app.post("/events", (req, res) => {
