3b84f79fa53c3cfba146359e887d0a4c2191252d
PreranaShrestha <prerana.sh@gmail.com>
2018-02-11 07:07:24 +0000

    Add events.js

---
+++ public/scripts/events.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+  const numberOfSlots = $('table').find('th').length - 1;
+  const $name = $('<input type="text" placeholder="name">');
+  const $tr = $('<tr>');
+  $tr.append($name);
+  for (let i = 0; i < numberOfSlots; i++) {
+    const $checkbox = $('<input type="checkbox" name="amount" />');
+    const $td = $('<td>').append($checkbox);
+    $td.append($checkbox);
+    $tr.append($td);
+  }
+  $('#slots').append($tr);
+
+  $('td').change(function() {
+    console.log (this.cellIndex);
+    console.log($(this).closest('tr').find('input[type="text"]').val());
+    console.log($(this).parents('table').find('tr:first-child').children('th:nth-child(' + (this.cellIndex + 1) + ')').html())
+
+  });
+});
+
