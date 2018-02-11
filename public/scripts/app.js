fa86b331f4fbcccc4efde49a86ebd9c15ff056a6
PreranaShrestha <prerana.sh@gmail.com>
2018-02-11 02:55:47 +0000

    Add pool

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -63,6 +63,17 @@ $(document).ready(function() {
     }
   });
 
+
+
+
+
+
+
+
+
+
+
+
   // Prevents default behavior of the form event submission and sets the data in a object to post to server using
   $('#eventForm').on('submit', (event) => {
 
